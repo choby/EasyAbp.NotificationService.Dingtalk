476cda8e30c9209d8ab32c60a34388763d636b36
niubiboy <choby@outlook.com>
2023-08-26 10:43:50 +0800

    更新一些文档

--- EasyAbp.NotificationService.Dingtalk/EasyAbp.NotificationService.Provider.Dingtalk.Abstractions/IAbpDingtalkServiceFactory.cs
+++ EasyAbp.NotificationService.Dingtalk/EasyAbp.NotificationService.Provider.Dingtalk.Abstractions/IAbpDingtalkServiceFactory.cs
@@ -4,10 +4,10 @@ namespace EasyAbp.NotificationService.Provider.Dingtalk;
 public interface IAbpDingtalkServiceFactory
 {
     /// <summary>
-    /// 使用本方法实例化一个微信服务
+    /// 使用本方法实例化一个钉钉服务
     /// </summary>
-    /// <param name="appId">目标微信应用的 appid，如果为空则取 Setting 中的默认值</param>
-    /// <typeparam name="TService">任意微信服务类型</typeparam>
+    /// <param name="appId">目标钉钉应用的 appid，如果为空则取 Setting 中的默认值</param>
+    /// <typeparam name="TService">任意钉钉`服务类型</typeparam>
     /// <returns></returns>
     Task<TService> CreateAsync<TService>(string appId = null) where TService : IAbpDingtalkService;
 }
